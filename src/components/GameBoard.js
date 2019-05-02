ff54912b9d05f6a2dbee3a868775abe0a3919581
mpreilly <mpreilly@gatech.edu>
2019-05-02 18:57:27 -0400

    Gets game from server (for hardcoded date)  and maps questions, logs them for correct tile clicks

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -9,34 +9,59 @@ class GameBoard extends Component {
         super(props);
 
         this.state = {
-            categories: [],
-            questions: {},
+            game: {},
+            questionsDone: new Set([])
 
         };
     }
 
-    questionChosen = (category, value) => {
+    componentDidMount() {
+        fetch('/games/2010-07-06', {method: 'GET'})
+            .then(data => data.json())
+            .then(json => this.setState({game: json}))
+    }   
+
+    loadGame = () => {
+
+    }
+
+    questionChosen = (round, category, value) => {
         console.log('Chosen: ' + category + " for " + value)
+        console.log(this.state.game[round][category][value]["question"])
+        this.setState(prevState => ({questionsDone: prevState.questionsDone.add(category + value)}))
+        if (this.state.questionsDone.size === 30) {
+            console.log("round over")
+        }
     }
 
     render () {
+        // if (!this.state.game.hasOwnProperty("jeopardy")) {
+        //     fetch('localhost:3001/dates')
+        //     .then((res) => console.log())
+        //     return ( 
+        //         <div>please choose game.</div>
+        //     )
+        // }
+        if (!this.state.game.hasOwnProperty("jeopardy")) {
+            return ( <div> loading game </div>)
+        }
         return (
             <Container style={{backgroundColor: "gray", marginTop: "0.5em", padding: "0.5em"}}>
                 <Row style={{display: "flex", flexWrap: "wrap", minHeight: "10em", marginBottom:"-3em"}}>
-                    {this.props.categories.map((catName) => {
+                    {Object.keys(this.state.game.jeopardy).map((catName) => {
                         return (<TopTile category={catName} />)
                     })}
                 </Row>
-                {[200, 400, 600, 800, 1000].map((value) => {
+                {["$200", "$400", "$600", "$800", "$1000"].map((value) => {
                     return (
                         <Row>
-                            {this.props.categories.map((catName) => {
+                            {Object.keys(this.state.game.jeopardy).map((catName) => {
                                 return (
                                     <Col>
                                         <button 
                                             className="question-tile" 
-                                            onClick={() => this.questionChosen(catName, value)}>
-                                            {value}
+                                            onClick={() => this.questionChosen("jeopardy", catName, value)}>
+                                            {this.state.questionsDone.has(catName + value) ? "" : value}
                                         </button>
                                     </Col>
                                 )
