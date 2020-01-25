f81355098b62e92184577f8c5f6f81b3696a6cde
mpreilly <mpreilly@gatech.edu>
2020-01-25 12:58:03 -0500

    Updates

---
+++ precache-manifest.14d1c5625f8c67da1bbf2a26a3bdc2cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f1e3115457910df38c96d410602060",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "91e4d84381c169858006",
+    "url": "/jeopardy/static/css/main.5cf0fa53.chunk.css"
+  },
+  {
+    "revision": "1f6257cfa4cf44fc5fe2",
+    "url": "/jeopardy/static/js/2.11f85bea.chunk.js"
+  },
+  {
+    "revision": "91e4d84381c169858006",
+    "url": "/jeopardy/static/js/main.5cde1928.chunk.js"
+  },
+  {
+    "revision": "62ac5bd5a1faa6c116fa",
+    "url": "/jeopardy/static/js/runtime~main.f29172a4.js"
+  }
+]);
